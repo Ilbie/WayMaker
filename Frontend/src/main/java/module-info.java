2758919d52971b07d2a9491db63a75b00e2d708b
ilbie <beancurd2022@gmail.com>
2024-06-11 03:43:40 +0900

    front update

---
+++ Frontend/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.waymaker.frontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.waymaker to javafx.fxml;
+    exports com.waymaker;
+}
